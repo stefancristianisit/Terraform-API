77db5bf44e363bc73e9dea558a5310bac9fe2bae
Cristian <cristian_isit@yahoo.co.uk>
2022-01-20 11:35:36 +0200

    modificari

--- main.tf
+++ main.tf
@@ -1,3 +1,63 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "=2.91.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+/*variable "ARM_SUBSCRIPTION_ID" {}
+variable "ARM_CLIENT_ID" {}
+variable "ARM_SECRET" {}
+variable "TENANT_ID" {} */
+
 provider "azurerm" {
-        version = "2.5.0"
+  features {}
+
+  # More information on the authentication methods supported by
+  # the AzureRM Provider can be found here:
+  # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
+
+  subscription_id = "${ARM_SUBSCRIPTION_ID}"
+  client_id       = "${ARM_CLIENT_ID}"
+  client_secret   = "${ARM_SECRET}"
+  tenant_id       = "${ARM_TENANT_ID}"
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "tf_test" {
+  name     = "test-resources"
+  location = "West Europe"
+}
+
+# Create a virtual network in the production-resources resource group
+/*resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
+} */
+
+resource "azurerm_container_group" "tf_container_test" {
+  name = "wheatherapi"
+  location =  azurerm_resource_group.tf_test.location
+  resource_group_name = azurerm_resource_group.tf_test.name
+  ip_address_type = "public"
+  dns_name_label      = "aplicatieAPI"
+  os_type             = "Linux"
+
+  container {
+    name   = "whaterapi"
+    image  = "mcr.microsoft.com/azuredocs/aci-helloworld:latest"
+    cpu    = "1"
+    memory = "1"
+
+  ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+
 }
